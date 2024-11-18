66edbe55efe3872235ed79cdbbebae628388e0f8
DereWah <160314410+Dere-Wah@users.noreply.github.com>
2024-11-18 11:15:37 +0100

    MY DIFF process_csgo_tar_files.py

--- diamond/src/process_csgo_tar_files.py
+++ diamond/src/process_csgo_tar_files.py
@@ -24,126 +24,95 @@ from data.dataset import Dataset, CSGOHdf5Dataset
 from data.episode import Episode
 from data.segment import SegmentId
 
+import os
 
-PREFIX = "hdf5_dm_july2021_"
+
+low_res_w = 64
+low_res_h = 48
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "tar_dir",
-        type=Path,
-        help="folder containing the .tar files from `dataset_dm_scraped_dust2_tars` folder on the OneDrive",
-    )
-    parser.add_argument(
-        "out_dir",
-        type=Path,
-        help="a new directory (should not exist already), the script will untar and process data there",
-    )
-    return parser.parse_args()
-
-
-def process_tar(path_tar: Path, out_dir: Path, remove_tar: bool) -> None:
-    d = path_tar.stem
-    assert path_tar.stem.startswith(PREFIX)
-    d = out_dir / "-".join(path_tar.stem[len(PREFIX) :].split("_to_"))
-    d.mkdir(exist_ok=False, parents=True)
-    shutil.move(path_tar, d)
-    subprocess.run(f"cd {d} && tar -xvf {path_tar.name}", shell=True)
-    new_path_tar = d / path_tar.name
-    if remove_tar:
-        new_path_tar.unlink()
-    else:
-        shutil.move(new_path_tar, path_tar.parent)
-
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"in_dir",
+		type=Path,
+		help="Specify a directory containing a list of .hdf5 files.",
+	)
+	parser.add_argument(
+		"out_dir",
+		type=Path,
+		help="Specify a directory where the dataset will be processed and saved.",
+	)
+	return parser.parse_args()
 
 def main():
-    args = parse_args()
+	args = parse_args()
+	
+	in_dir = args.in_dir.absolute()
+	out_dir = args.out_dir.absolute()
 
-    tar_dir = args.tar_dir.absolute()
-    out_dir = args.out_dir.absolute()
+	with Path("test_split.txt").open("r") as f:
+		test_files = f.read().split("\n")
 
-    if not tar_dir.exists():
-        print(
-            "Wrong usage: the tar directory should exist (and contain the downloaded .tar files)"
-        )
-        return
+	full_res_dir = out_dir / "full_res"
+	low_res_dir = out_dir / "low_res"
+	
+	os.mkdir(full_res_dir)
 
-    if out_dir.exists():
-        print(f"Wrong usage: the output directory should not exist ({args.out_dir})")
-        return
+	#
+	# Create low-res data
+	#
+	
+	for file_name in os.listdir(in_dir):
+		file_path = os.path.join(in_dir, file_name)
+		print(file_path)
+		if os.path.isfile(file_path):
+			shutil.copy(file_path, full_res_dir)
+	
+	csgo_dataset = CSGOHdf5Dataset(full_res_dir)
 
-    with Path("test_split.txt").open("r") as f:
-        test_files = f.read().split("\n")
+	train_dataset = Dataset(low_res_dir / "train", None)
+	test_dataset = Dataset(low_res_dir / "test", None)
+	original_size = None
+	num_actions = 0
 
-    full_res_dir = out_dir / "full_res"
-    low_res_dir = out_dir / "low_res"
+	for i in tqdm(csgo_dataset._filenames, desc="Creating low_res"):
+		episode = Episode(
+			**{
+				k: v
+				for k, v in csgo_dataset[SegmentId(i, 0, 1000)].__dict__.items()
+				if k not in ("mask_padding", "id")
+			}
+		)
+		
+		if not original_size:
+			original_size = episode.obs.size()
+		
+		episode.obs = T.resize(
+			episode.obs, (low_res_h, low_res_w), interpolation=T.InterpolationMode.BICUBIC
+		)
+		num_actions = episode.act.size()
+		filename = csgo_dataset._filenames[i]
+		file_id = f"{filename.parent.stem}/{filename.name}"
+		episode.info = {"original_file_id": file_id}
+		dataset = test_dataset if filename.name in test_files else train_dataset
+		dataset.add_episode(episode)
 
-    tar_files = [
-        x for x in tar_dir.iterdir() if x.suffix == ".tar" and x.stem.startswith(PREFIX)
-    ]
-    n = len(tar_files)
+	train_dataset.save_to_default_path()
+	test_dataset.save_to_default_path()
 
-    if (
-        n < 28
-        and input(
-            f"Found only {n} .tar files instead of 28, so it looks like the data has not been entirely downloaded, continue? [y|N] "
-        )
-        != "y"
-    ):
-        return
+	print(
+		f"Split train/test data ({train_dataset.num_episodes}/{test_dataset.num_episodes} episodes)\n"
+	)
 
-    str_files = "\n".join(map(str, tar_files))
-    print(f"Ready to untar {n} tar files:\n{str_files}")
-
-    remove_tar = (
-        input("Remove .tar files once they are processed? [y|N] ").lower() == "y"
-    )
-
-    # Untar CSGO files
-    f = partial(process_tar, out_dir=full_res_dir, remove_tar=remove_tar)
-    with Pool(n) as p:
-        p.map(f, tar_files)
-
-    print(f"{n} .tar files unpacked in {full_res_dir}")
-
-    #
-    # Create low-res data
-    #
-
-    csgo_dataset = CSGOHdf5Dataset(full_res_dir)
-
-    train_dataset = Dataset(low_res_dir / "train", None)
-    test_dataset = Dataset(low_res_dir / "test", None)
-
-    for i in tqdm(csgo_dataset._filenames, desc="Creating low_res"):
-        episode = Episode(
-            **{
-                k: v
-                for k, v in csgo_dataset[SegmentId(i, 0, 1000)].__dict__.items()
-                if k not in ("mask_padding", "id")
-            }
-        )
-        episode.obs = T.resize(
-            episode.obs, (30, 56), interpolation=T.InterpolationMode.BICUBIC
-        )
-        filename = csgo_dataset._filenames[i]
-        file_id = f"{filename.parent.stem}/{filename.name}"
-        episode.info = {"original_file_id": file_id}
-        dataset = test_dataset if filename.name in test_files else train_dataset
-        dataset.add_episode(episode)
-
-    train_dataset.save_to_default_path()
-    test_dataset.save_to_default_path()
-
-    print(
-        f"Split train/test data ({train_dataset.num_episodes}/{test_dataset.num_episodes} episodes)\n"
-    )
-
-    print("You can now edit `config/env/csgo.yaml` and set:")
-    print(f"path_data_low_res: {low_res_dir}")
-    print(f"path_data_full_res: {full_res_dir}")
+	print("You can now edit `config/env/csgo.yaml` and set:")
+	print(f"path_data_low_res: {low_res_dir}")
+	print(f"path_data_full_res: {full_res_dir}")
+	print(f"Your FULL RES SIZE is {original_size} (format: [value, channel, HEIGHT, WIDTH] you only care about last 2), and your low_res size is W: {low_res_w} H:{low_res_h}")
+	print("in config/env/csgo.yaml put your FULL RES SIZE [width, height];" + 
+			"in config/agent/csgo.yaml set the upsampler.upsampling_factor to the factor to upsample from low_res to full_res.")
+	print(f"Also your number of actions is {num_actions}")
 
 
 if __name__ == "__main__":
-    main()
+	main()
