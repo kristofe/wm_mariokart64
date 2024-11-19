9df13688d7e5f8ecdc6748bcb84c109f85f07207
Dere-Wah <derexcontact@gmail.com>
2024-11-19 10:31:53 +0100

    doc(play): add comment for changing path

--- diamond/src/play.py
+++ diamond/src/play.py
@@ -37,7 +37,7 @@ def check_args(args: argparse.Namespace) -> None:
 def prepare_play_mode(cfg: DictConfig, args: argparse.Namespace) -> PlayEnv:
 
 	path_hf =  Path(snapshot_download(repo_id="DereWah/diamond-mariokart64", allow_patterns="csgo/*"))
-
+	# If you're running with a model that you already downloaded. simply change this path above to match the folder in which you put the model
 	path_ckpt = path_hf / "csgo/model/csgo.pt"
 	spawn_dir = path_hf / "csgo/spawn"
 
